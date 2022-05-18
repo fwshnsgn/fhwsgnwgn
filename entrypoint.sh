bbb5538fff8e1e0b847ecbe09bdeab8f3c2b5dbd
blaufurenos <81584208+blaufurenos@users.noreply.github.com>
2022-05-18 23:14:22 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-rclone version
+ss version
 mkdir /.config
 mkdir /.config/rclone/
 wget "$CONFIG_IN_URL" -O /.rclone.conf
-rclone listremotes
-rclone serve http $CLOUDNAME: --addr :$PORT --vfs-read-chunk-size 128M
+ss listremotes
+ss serve http $CLOUDNAME: --addr :$PORT --vfs-read-chunk-size 128M
