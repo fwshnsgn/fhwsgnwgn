069f396efa7e1da99d60cfec839f17ea6d0f1e90
blaufurenos <81584208+blaufurenos@users.noreply.github.com>
2022-05-27 03:26:45 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ mkdir /.config
 mkdir /.config/rclone/
 wget "$CONFIG_IN_URL" -O /.rclone.conf
 ss listremotes
-ss serve http $CLOUDNAME: --addr :$PORT --no-modtime
+ss serve http $CLOUDNAME: --addr :$PORT --no-modtime --dir-cache-time 48h
